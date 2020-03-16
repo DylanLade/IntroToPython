12bc86dddbbd3388fd1c0084ebd4d42354b3c510
Dylan Lade <dylanlade98@gmail.com>
2020-03-16 17:34:43 -0500

    Changed Arrays1 to Lists1

--- Lists1/lists1.py
+++ Lists1/lists1.py
@@ -1,6 +1,6 @@
 # arrays1 assignment by Dylan Lade for Python Programming 1
 
-def display_array(label, items):
+def display_list(label, items):
     print(label)
     for item in items:
         print(item)
@@ -9,29 +9,29 @@ foods = ["pizza", "salad", "hamburger", "steak", "apple", "orange"]
 
 def main():
 # Display the original array 
-    display_array("foods in origional order: ", foods)
+    display_list("foods in origional order: ", foods)
 # Display the sorted array   
     foods.sort()
-    display_array("foods in ascending alphabetical order: ", foods)
+    display_list("foods in ascending alphabetical order: ", foods)
 # Display the reverse sorted array    
     foods.sort(reverse=True)
-    display_array("foods in descending alphabetical order: ", foods)
+    display_list("foods in descending alphabetical order: ", foods)
 # Copy the orignal array and sort the copy
     foods2 = foods.copy()
     foods2.sort()
-    display_array("foods2 in ascending alphabetical order: ", foods2)
+    display_list("foods2 in ascending alphabetical order: ", foods2)
 # Show the original array still in reverse order
-    display_array("foods still in descending alphabetical order: ", foods)
+    display_list("foods still in descending alphabetical order: ", foods)
 # Reverse the decending array to make it sorted
     foods.reverse()
-    display_array("foods in ascending alphabetical order: ", foods)
+    display_list("foods in ascending alphabetical order: ", foods)
 # Append the new items to the sorted array
     foods.append("carrots")
     foods.append("milk")
-    display_array("sorted foods with carrots and milk appended to end: ", foods)
+    display_list("sorted foods with carrots and milk appended to end: ", foods)
 # Sort the array with the new items 
     foods.sort()
-    display_array("foods in ascending alphabetical order: ", foods)
+    display_list("foods in ascending alphabetical order: ", foods)
 # Find the index of pizza
     pizza_index = foods.index("pizza")
     print("Pizza is at " + str(pizza_index))
