ba26dcce305311f1d29a59d4b0237f9d4d4ebb08
Dylan Lade <dylanlade98@gmail.com>
2020-04-15 16:46:19 -0500

    Interview questions Done

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-val = input("Whats your name? : ")
+sample = dict()
 
-print("Is " + val + " right?")
+with open("test.txt", "r") as a_file:
+    for line in a_file:
+        for word in line.split():
+            if word in sample:
+                sample[word] = sample[word] + 1
+            else:
+                sample[word] = 1
+
+print(sample)
